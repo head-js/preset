18858689473a86f43ace5f57e9e933713665f3d3
hbrls <shuaizhexu@gmail.com>
2024-03-18 23:27:52 +0800

    !!! react & axios

--- packages/react/test/App.jsx
+++ packages/react/test/App.jsx
@@ -1,16 +1,39 @@
 import React, { useState } from 'react';
+import $http from '@head/http';
 import Footer from './Footer';
 
 
+async function doGet(status) {
+  const { code, message, data, res } = await $http.get(`https://httpbin.org/status/${status}`, { k1: 'v1' });
+  console.log(res);
+}
+
+
+async function doPost(status) {
+  try {
+    const { code, message, data, res } = await $http.post(`https://httpbin.org/status/${status}`, { k1: 'v1' }, { k2: 'v2' });
+    console.log(res);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
 function App() {
-  const [ message, setMessage ] = useState('World');
+  const [ status, setStatus ] = useState('200');
 
   return (
     <div className="App h-screen w-screen pt-8 bg-gray-100 prose prose-neutral">
-      <h1 className="p-4 text-center">Hello {message}!</h1>
+      <h1 className="p-4 text-center">Hello {status}!</h1>
       <form>
-        <div className="form-group text-center">
-          <input type="text" className="form-input px-4 py-3 rounded-full" onChange={(e) => setMessage(e.target.value)} />
+        <div className="form-group mb-4 text-center">
+          <input type="text" className="form-input px-4 py-3 rounded-full" onChange={(e) => setStatus(e.target.value)} />
+        </div>
+        <div className="form-group mb-4 text-center">
+          <button type="button" className="form-input px-4 py-3 bg-gray-200 rounded-xl" onClick={() => doGet(status)}>doGet</button>
+        </div>
+        <div className="form-group mb-4 text-center">
+          <button type="button" className="form-input px-4 py-3 bg-gray-200 rounded-xl" onClick={() => doPost(status)}>doPost</button>
         </div>
       </form>
       <Footer />
